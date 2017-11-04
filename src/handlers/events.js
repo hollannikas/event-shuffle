4abca2cdb60962ec991db15785adb9d5028d7812
hollannikas <hollannikas@users.noreply.github.com>
2017-11-04 19:53:38 +0200

    Added postgres, sequelize and some kind of model

--- src/handlers/events.js
+++ src/handlers/events.js
@@ -1,1 +1,43 @@
-export const helloWorld = (request, reply) => reply('hello world');
+import { Sequelize, DataTypes } from "sequelize";
+
+const sequelize = new Sequelize(process.env.DATABASE_URL);
+
+const Event = sequelize.define('Event', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: DataTypes.TEXT,
+    dates: DataTypes.ARRAY(DataTypes.DATEONLY)
+});
+
+Event.sync({force: false});
+
+function getAll() {
+    return Event.findAll({attributes: ['id', 'name']})
+        .then(event => event)
+        .catch(err => {
+            console.log(err);
+            return {error: err};
+        });
+}
+
+function findById(id) {
+    return Event.findById(id, {attributes: ['id', 'name', 'dates']})
+        .then(event => event)
+        .catch(err => {
+            console.log(err);
+            return {error: err};
+        });
+}
+
+function create(event) {
+    console.log(event);
+    return Event.create(event)
+        .then(event => {return {id: event.id}})
+        .catch(err => {
+            console.log(err);
+            return {error: err};
+        });
+}
+
+export const eventList = (request, reply) => reply(getAll());
+export const event = (request, reply) => reply(findById(request.params.id));
+export const createEvent = (request, reply) => reply(create(request.payload));
--- src/routes/events.js
+++ src/routes/events.js
@@ -1,5 +1,11 @@
-import { helloWorld } from '../handlers/events';
+import {
+    event,
+    eventList,
+    createEvent
+} from '../handlers/events';
 
 export const routes = [
-    { method: 'GET', path: '/hello', handler: helloWorld }
+    { method: 'GET', path: '/event/list', handler: eventList },
+    { method: 'GET', path: '/event/{id}', handler: event },
+    { method: 'POST', path: '/event', handler: createEvent }
 ];
