1fe429347b76cd39cbfc6fe4d973b30d901f0137
hollannikas <hollannikas@users.noreply.github.com>
2017-11-05 10:52:25 +0200

    Added possible dates with votes to model

--- src/handlers/events.js
+++ src/handlers/events.js
@@ -1,14 +1,24 @@
-import { Sequelize, DataTypes } from "sequelize";
+import {Sequelize, DataTypes} from "sequelize";
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {dialect: 'postgres'} );
+const sequelize = new Sequelize(process.env.DATABASE_URL, {dialect: 'postgres'});
 
 const Event = sequelize.define('Event', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     name: DataTypes.TEXT,
-    dates: DataTypes.ARRAY(DataTypes.DATEONLY)
+    dates: DataTypes.ARRAY(DataTypes.STRING)
 });
 
-Event.sync({force: false});
+const Vote = sequelize.define('Vote', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    date: DataTypes.DATEONLY,
+    people: DataTypes.ARRAY(DataTypes.STRING)
+});
+
+Event.hasMany(Vote, {as: 'votes'});
+
+Event.sync({force: true})
+    .then(() => Vote.sync({force: true}));
+
 
 function getAll() {
     return Event.findAll({attributes: ['id', 'name']})
@@ -20,8 +30,16 @@ function getAll() {
 }
 
 function findById(id) {
-    return Event.findById(id, {attributes: ['id', 'name', 'dates']})
-        .then(event => event)
+    return Event.findById(id,
+        {
+            attributes: ['id', 'name', 'dates'],
+            include: [{
+                model: Vote,
+                as: 'votes',
+                attributes: ['date', 'people']
+            }]
+        })
+        .then(event => event ? event : "Not found")
         .catch(err => {
             console.log(err);
             return {error: err};
@@ -29,9 +47,10 @@ function findById(id) {
 }
 
 function create(event) {
-    console.log(event);
     return Event.create(event)
-        .then(event => {return {id: event.id}})
+        .then(event => {
+            return {id: event.id};
+        })
         .catch(err => {
             console.log(err);
             return {error: err};
@@ -40,4 +59,4 @@ function create(event) {
 
 export const eventList = (request, reply) => reply(getAll());
 export const event = (request, reply) => reply(findById(request.params.id));
-export const createEvent = (request, reply) => reply(create(request.payload));
+export const createEvent = (request, reply) => reply(create(request.payload)).code(201);
